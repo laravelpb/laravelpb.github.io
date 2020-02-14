9dd7a28e8930ece522435d57486fee2d35b7831d
Vikash pathak <vikashh.pathak@gmail.com>
2020-02-15 04:47:32 +0530

    Updated commmunity information.

---
+++ precache-manifest.2007e8d0f73d08190ea6004ddee1cc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75675c64ae3b0938686c3a20fe31cebe",
+    "url": "https://laravelpb.github.io/index.html"
+  },
+  {
+    "revision": "9df5c8915ea3b322f72d",
+    "url": "https://laravelpb.github.io/static/css/main.66ab4137.chunk.css"
+  },
+  {
+    "revision": "c299743621afa63d4c4e",
+    "url": "https://laravelpb.github.io/static/js/2.c696c082.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "https://laravelpb.github.io/static/js/2.c696c082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df5c8915ea3b322f72d",
+    "url": "https://laravelpb.github.io/static/js/main.49842aeb.chunk.js"
+  },
+  {
+    "revision": "04a9a87abfc76bb445d0",
+    "url": "https://laravelpb.github.io/static/js/runtime-main.ee7ba5c6.js"
+  }
+]);
