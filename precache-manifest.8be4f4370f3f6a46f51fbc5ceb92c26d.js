79612303fc060c04ed52fa9c81e46f9b53ecf48b
Vikash Pathak <vikashh.pathak@gmail.com>
2022-10-25 13:14:55 +0530

    Updated new details on site.

---
+++ precache-manifest.8be4f4370f3f6a46f51fbc5ceb92c26d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1a985d84f476f140a070620082a526",
+    "url": "https://laravelpb.github.io/index.html"
+  },
+  {
+    "revision": "aa29c2ca3a61232d8960",
+    "url": "https://laravelpb.github.io/static/css/main.87279c8e.chunk.css"
+  },
+  {
+    "revision": "9a37b1c39cb4319083f5",
+    "url": "https://laravelpb.github.io/static/js/2.275b5306.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://laravelpb.github.io/static/js/2.275b5306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa29c2ca3a61232d8960",
+    "url": "https://laravelpb.github.io/static/js/main.e9f0dd57.chunk.js"
+  },
+  {
+    "revision": "70c953fb2c82b6cc8419",
+    "url": "https://laravelpb.github.io/static/js/runtime-main.e3062110.js"
+  }
+]);
